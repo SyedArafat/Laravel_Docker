0628b6abb90b46af4e3f890074c52543fea2b2c0
Arafat <syedyeaminarafat@gmail.com>
2020-02-25 14:57:19 +0600

    database connection established

---
+++ app/Http/Controllers/FirstController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FirstController extends Controller
+{
+    public function __construct()
+    {
+
+    }
+
+    public function index()
+    {
+        return "Hello World";
+    }
+}
